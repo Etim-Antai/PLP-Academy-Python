674f96f621055b1982368ced87a5b10b9ad521d0
Etim-Antai <etymantai@gmail.com>
2024-10-31 22:44:53 +0100

    week on assignment submitted

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# WEEK ONE ASSIGNMENT
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your Location: ")
+
+print("Welcome ", name, ", You are ", age, "years old and you live in", location, end = "." )
